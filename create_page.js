af378037528f4de35b34b275278a404305776c1f
bokwoon <bokwoon.c@gmail.com>
2021-04-23 21:40:31 +0800

    checkpoint

--- create_page.js
+++ create_page.js
@@ -11,11 +11,30 @@ document.addEventListener("DOMContentLoaded", function () {
     redirect: document.querySelector("#redirect-group"),
     disabled: document.querySelector("#disabled-group"),
   };
+  const themePath = document.querySelector("#pm-theme-path");
+  if (!themePath) {
+    throw new Error("#pm-theme-path not found");
+  }
+  const themes = {};
+  for (const opt of themePath?.children) {
+    if (!(opt instanceof HTMLOptionElement)) {
+      continue;
+    }
+    themes[opt.value] = document.getElementById(`theme:${opt.value}`);
+  }
+  window.themes = themes;
   function render() {
     for (const [name, group] of Object.entries(groups)) {
       group.hidden = name !== selectPageType.value;
     }
+    for (const opt of themePath.children) {
+      if (!(opt instanceof HTMLOptionElement)) {
+        continue;
+      }
+      themes[opt.value].hidden = !(selectPageType.value === "template" && opt.selected);
+    }
   }
   render();
   selectPageType.addEventListener("input", render);
+  themePath.addEventListener("input", render);
 });
